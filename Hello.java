b33150f8c4371406bec3a17c4b433d1f9bf8db8d
unknown <h278893@gmail.com>
2023-06-11 00:24:49 +0530

    First Commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Hello{
+    public static void main (String a[]){
+
+        int num =212, num2 =23;
+        float n=32.3f;
+        byte b=127;
+        short sh=558;
+        long l=3423424552l;
+        System.out.println("Hello world "+(num+num2));
+        System.out.println(23+23);
+        System.out.println(n);
+        System.out.println(l);
+    }
+}
