f06c8c990d423486361e186f96a98a52184c62d0
honey770 <h278893@gmail.com>
2023-06-14 18:48:55 +0530

    hoo

---
+++ array1.java
@@ -0,0 +1,20 @@
+public class array1 {
+
+    public static void main(String args[]){
+
+        int num[]={324,356,3,677,7,8,89};
+        int num1[]=new int[4]; //Dinamicaly 
+
+        num1[1]=3224;
+
+        for(int i=0;i<4;i++){
+
+            System.out.print(num1[i]+" ");
+        }
+
+        
+
+
+    }
+    
+}
