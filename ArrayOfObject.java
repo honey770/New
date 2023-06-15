4f73f015b45e516dd2332ece8568b59c0157cd86
honey770 <h278893@gmail.com>
2023-06-15 19:36:58 +0530

    s

---
+++ ArrayOfObject.java
@@ -0,0 +1,19 @@
+class Student{
+
+    int rollno;
+    String name;
+    int marks;
+
+}
+
+public class ArrayOfObject {
+
+    public static void main(String args[]){
+
+        Student s1=new Student();
+        Student s[]=new Student[3];
+
+        System.out.print(s1);
+    }
+    
+}
